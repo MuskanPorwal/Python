7cf3083858f6e92a6b4595d6525542426362a5f9
Muskan Porwal <porwalmuskan2411@gmail.com>
2025-01-16 15:36:47 -0600

    Updated python file

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -32,27 +32,6 @@ print(check_win(get_choices()["player"], get_choices()["computer"]))
 
 
 
-#def greeting():
-#    return "HI"
-
-#print(greeting())
-
-
-#choices = get_choices()
-#print(choices)
-
-#distcionary is a key value pair where the value can be a variable
-#dict = {"name": "beau", "color": "blue"}
-
- #Librarires are a set of useful functions without writing additional functions 
-#Lists are used to store multiple itesm in a single variable
-
-#food = ["pizza", "carrots", "eggs"]
-#dinner = random.choice(food) #to choose a random item from teh list
-
-# combining strings usin fstring method 
-#age = 25 
-# print (f"Jim is {age} years old")
 
 
 
